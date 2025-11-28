827e732f551e5bcb383b49346f40438604808d95
Sukhmani <thukralsukhmani@cityuniversity.edu>
2025-11-27 17:20:34 -0800

    stact 2 updated

--- stack2/main.tf
+++ stack2/main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
-  region = "us-west-2"  
+  region = "us-west-2"
 }
 
 module "bedrock_kb" {
-  source = "../modules/bedrock_kb" 
+  source = "../modules/bedrock_kb"
 
   knowledge_base_name        = "my-bedrock-kb"
   knowledge_base_description = "Knowledge base connected to Aurora Serverless database"
 
-  aurora_arn        = "" #TODO Update with output from stack1
-  aurora_db_name    = "myapp"
-  aurora_endpoint   = "" # TODO Update with output from stack1
-  aurora_table_name = "bedrock_integration.bedrock_kb"
-  aurora_primary_key_field = "id"
-  aurora_metadata_field = "metadata"
-  aurora_text_field = "chunks"
-  aurora_verctor_field = "embedding"
-  aurora_username   = "dbadmin"
-  aurora_secret_arn = "" #TODO Update with output from stack1
-  s3_bucket_arn = "" #TODO Update with output from stack1
+  aurora_arn                 = "arn:aws:rds:us-west-2:245403145430:cluster:my-aurora-serverless"
+  aurora_db_name             = "myapp"
+  aurora_endpoint            = "my-aurora-serverless.cluster-cfpcpqajvwgv.us-west-2.rds.amazonaws.com"
+  aurora_table_name          = "bedrock_integration.bedrock_kb"
+  aurora_primary_key_field   = "id"
+  aurora_metadata_field      = "metadata"
+  aurora_text_field          = "chunks"
+  aurora_verctor_field       = "embedding"
+  aurora_username            = "dbadmin"
+  aurora_secret_arn          = "arn:aws:secretsmanager:us-west-2:245403145430:secret:my-aurora-serverless-rzyen9"
+  s3_bucket_arn              = "arn:aws:s3:::bedrock-kb-245403145430"
 }
